ebb2a4fec30684237b47ee47a0c39dcbe1082f8b
Muhammad Abdullah <pxga.abdullah@hotmail.com>
2025-10-01 18:14:45 +0600

    added blog controllers

---
+++ src/modules/blog/post.controller.ts
@@ -0,0 +1,71 @@
+import { type Request, type Response } from "express";
+import { PostService, type iGetAllPostsParams } from "./post.service.js";
+
+const createPost = async (req: Request, res: Response) => {
+  try {
+    const result = await PostService.createPost(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+const getAllPosts = async (req: Request, res: Response) => {
+  try {
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const search = (req.query.search as string) || "";
+    const isFeatured = req.query.isFeatured
+      ? req.query.isFeatured === "true"
+      : undefined;
+    const tags = req.query.tags ? (req.query.tags as string).split(",") : [];
+
+    const result = await PostService.getAllPosts({
+      page,
+      limit,
+      search,
+      isFeatured,
+      tags,
+    } as iGetAllPostsParams);
+
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch posts", details: err });
+  }
+};
+
+const getPostById = async (req: Request, res: Response) => {
+  const post = await PostService.getPostById(req.params.id!);
+  if (!post) return res.status(404).json({ error: "Post not found" });
+  res.json(post);
+};
+
+const updatePost = async (req: Request, res: Response) => {
+  const post = await PostService.updatePost(req.params.id!, req.body);
+  res.json(post);
+};
+
+const deletePost = async (req: Request, res: Response) => {
+  await PostService.deletePost(req.params.id!);
+  res.json({ message: "Post deleted" });
+};
+
+/*
+const getBlogStat = async (req: Request, res: Response) => {
+  try {
+    const result = await PostService.getBlogStat();
+    res.json(result);
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch stats", details: err });
+  }
+};
+*/
+
+export const PostController = {
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+  // getBlogStat,
+};
