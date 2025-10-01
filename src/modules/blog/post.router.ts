cc4c2f7c60ca7e3884c8cbe35e9609e3085b20a0
Muhammad Abdullah <pxga.abdullah@hotmail.com>
2025-10-01 18:15:05 +0600

    added blog routes

---
+++ src/modules/blog/post.router.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import { PostController } from "./post.controller.js";
+
+const router = express.Router();
+// router.get("/stats", PostController.getBlogStat);
+
+router.post("/", PostController.createPost);
+
+router.get("/", PostController.getAllPosts);
+router.get("/:id", PostController.getPostById);
+router.patch("/:id", PostController.updatePost);
+router.delete("/:id", PostController.deletePost);
+
+export const postRouter = router;
