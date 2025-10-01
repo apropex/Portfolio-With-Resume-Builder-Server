30f2005e20f5de725dc01653b7842d399482a67c
Muhammad Abdullah <pxga.abdullah@hotmail.com>
2025-10-01 18:14:25 +0600

    added blog services

---
+++ src/modules/blog/post.service.ts
@@ -0,0 +1,168 @@
+import type { Blog, Prisma } from "@prisma/client";
+import prisma, { PrismaOptions } from "../../config/db.js";
+
+const { FindUnique, Delete, FindMany, Create, Update, Count } = PrismaOptions(
+  prisma.blog,
+);
+
+export interface iGetAllPostsParams {
+  page?: number;
+  limit?: number;
+  search?: string;
+  isFeatured?: boolean;
+  tags?: string[];
+}
+
+const createPost = async (payload: Prisma.BlogCreateInput): Promise<Blog> => {
+  const result = await Create({
+    data: payload,
+    include: {
+      author: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+
+  return result;
+};
+
+const getAllPosts = async ({
+  page = 1,
+  limit = 10,
+  search,
+  isFeatured,
+  tags,
+}: iGetAllPostsParams) => {
+  const skip = (page - 1) * limit;
+
+  const where: any = {
+    AND: [
+      search && {
+        OR: [
+          { title: { contains: search, mode: "insensitive" } },
+          { content: { contains: search, mode: "insensitive" } },
+        ],
+      },
+      typeof isFeatured === "boolean" && { isFeatured },
+      tags && tags.length > 0 && { tags: { hasEvery: tags } },
+    ].filter(Boolean),
+  };
+
+  const result = await FindMany({
+    skip,
+    take: limit,
+    where,
+    include: {
+      author: true,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const total = await Count({ where });
+
+  return {
+    data: result,
+    pagination: {
+      page,
+      limit,
+      total,
+      totalPages: Math.ceil(total / limit),
+    },
+  };
+};
+
+const getPostById = async (id: string) => {
+  // return await prisma.$transaction(async (tx) => {
+  //   return await tx.blog.findUnique({
+  //     where: { id },
+  //     include: { author: true },
+  //   });
+  // });
+
+  return await FindUnique({
+    where: { id },
+    include: {
+      author: {
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      },
+    },
+  });
+};
+
+const updatePost = async (id: string, data: Partial<any>) => {
+  return Update({ where: { id }, data });
+};
+
+const deletePost = async (id: string) => {
+  return Delete({ where: { id } });
+};
+
+/*
+const getBlogStat = async () => {
+    return await prisma.$transaction(async (tx) => {
+        const aggregates = await tx.blog.aggregate({
+            _count: true,
+            _sum: { views: true },
+            _avg: { views: true },
+            _max: { views: true },
+            _min: { views: true },
+        })
+
+        const featuredCount = await tx.blog.count({
+            where: {
+                isFeatured: true
+            }
+        });
+
+        const topFeatured = await tx.blog.findFirst({
+            where: { isFeatured: true },
+            orderBy: { views: "desc" }
+        })
+
+        const lastWeek = new Date();
+        lastWeek.setDate(lastWeek.getDate() - 7)
+
+        const lastWeekBlogCount = await tx.blog.count({
+            where: {
+                createdAt: {
+                    gte: lastWeek
+                }
+            }
+        })
+
+        return {
+            stats: {
+                totalPosts: aggregates._count ?? 0,
+                totalViews: aggregates._sum.views ?? 0,
+                avgViews: aggregates._avg.views ?? 0,
+                minViews: aggregates._min.views ?? 0,
+                maxViews: aggregates._max.views ?? 0
+            },
+            featured: {
+                count: featuredCount,
+                topPost: topFeatured,
+            },
+            lastWeekPostCount
+        };
+    })
+}
+    */
+
+export const PostService = {
+  createPost,
+  getAllPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+  // getBlogStat
+};
