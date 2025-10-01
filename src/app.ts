7899d4cb9a543655a8b8c5e1816a34681421f89b
Muhammad Abdullah <pxga.abdullah@hotmail.com>
2025-10-01 18:09:40 +0600

    configure app and server

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import compression from "compression";
+import cors from "cors";
+import express from "express";
+import { authRouter } from "./modules/auth/auth.routes.js";
+import { postRouter } from "./modules/blog/post.router.js";
+import { userRouter } from "./modules/user/user.routes.js";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/blog", postRouter);
+app.use("/api/v1/auth", authRouter);
+
+// Default route for testing
+app.get("/", (_req, res) => {
+  res.send("API is running");
+});
+
+// 404 Handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+export default app;
